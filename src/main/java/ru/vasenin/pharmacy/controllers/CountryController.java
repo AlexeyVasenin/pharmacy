7fe9c4996ff7037ee981cb84ff4ee7df232134d0
Alexey <alexxx1809@gmail.com>
2021-05-23 19:13:03 +0500

    Убрал не используемые зависимости

--- src/main/java/ru/vasenin/pharmacy/controllers/CountryController.java
+++ src/main/java/ru/vasenin/pharmacy/controllers/CountryController.java
@@ -1,6 +1,5 @@
 package ru.vasenin.pharmacy.controllers;
 
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
